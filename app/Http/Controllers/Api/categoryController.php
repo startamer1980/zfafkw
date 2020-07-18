c09d421626d1578d3b5fd2e54169daa1c3e11d09
tamer <star.tamer.1980@gmail.com>
2020-07-18 09:25:09 +0200

    add cards

--- app/Http/Controllers/Api/categoryController.php
+++ app/Http/Controllers/Api/categoryController.php
@@ -9,11 +9,18 @@ use Illuminate\Http\Request;
 class categoryController extends Controller
 {
 
-    public function get_main_category(){
+    public function get_main_categodry(){
         $result = Category::getAllMainCategory()->paginate(PAGINATION_API_COUNT);
 //        return $result;
 //        return response()->json($result);
-        return response()->json($result);
+        // More info about toArray() method, see: vendor\laravel\framework\src\Illuminate\Pagination\LengthAwarePaginator.php file
+        $resultArray = $result->toArray();
+
+// Now, what you need is the $result["data"] element
+        $categories = $resultArray["data"];
+        unset($resultArray['data']);
+        $resultArray['categories'] = $categories;
+        return response()->json($resultArray);
     }
     public function get_sub_category($cat_id){
         $sub_categories = Category::getAllSubCategory($cat_id)->paginate(PAGINATION_API_COUNT);
